0d7341c445cd644bf7ba7d1b6ecead76b81015b3
thomas-crane <thomasgcrane@gmail.com>
2020-04-23 11:32:02 +1000

    feat(tp_rec): add alpine build script

---
+++ throughput_recorder/alpine-build.sh
@@ -0,0 +1,6 @@
+docker run \
+  --rm \
+  -v "$PWD":/usr/src/throughput_recorder \
+  -w /usr/src/throughput_recorder \
+  rust:1.42.0-alpine \
+  cargo build --target x86_64-unknown-linux-musl --release
