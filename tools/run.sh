e28b987a78249bb75904ebbba7efbc04f3b8ce6d
thomas-crane <thomasgcrane@gmail.com>
2020-05-09 23:57:25 +1000

    feat(tools): add run tool

---
+++ tools/run.sh
@@ -0,0 +1,19 @@
+if [[ ! $(basename $PWD) == "tools" ]]; then
+  echo "run.sh must be run from the tools directory"
+  exit 1
+fi
+
+if [[ ! $# == 4 ]]; then
+  echo "Usage: MODE COUNT RATE PAYLOAD_SIZE"
+  exit 1
+fi
+
+# set the IPs.
+source `realpath ./set-ips.sh`
+
+# start the proxy and the consumer.
+docker exec -e PRODUCER_IP -e CONSUMER_IP -d proxy proxy_server $1
+docker exec -d consumer traffic_consumer $1
+
+# start the producer, don't exec in detached mode (-d) because we need to wait for it to finish.
+docker exec -e PROXY_IP producer traffic_producer $1 $2 $3 $4
