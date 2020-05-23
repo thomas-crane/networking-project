b61295f926f000295e0f52f4c56ae8abe3aafd43
thomas-crane <thomasgcrane@gmail.com>
2020-05-23 11:31:37 +1000

    feat(tools): add delay to run for startup time

--- tools/run.sh
+++ tools/run.sh
@@ -5,6 +5,7 @@ fi
 
 if [[ ! $# == 4 ]]; then
   echo "Usage: MODE COUNT RATE PAYLOAD_SIZE"
+  echo "  E.g: tcp  100   10   10000"
   exit 1
 fi
 
@@ -14,6 +15,8 @@ source `realpath ./set-ips.sh`
 # start the proxy and the consumer.
 docker exec -e PRODUCER_IP -e CONSUMER_IP -d proxy proxy_server $1
 docker exec -d consumer traffic_consumer $1
+# allow them to start for a second.
+sleep 1
 
 # start the producer, don't exec in detached mode (-d) because we need to wait for it to finish.
 docker exec -e PROXY_IP producer traffic_producer $1 $2 $3 $4
