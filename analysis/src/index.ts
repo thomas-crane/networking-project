ca78d32fe0c31bffb59274e84efe5ceb89f47fe4
thomas-crane <thomasgcrane@gmail.com>
2020-06-26 21:10:28 +1000

    feat(analysis): produce all graphs in one go

--- analysis/src/index.ts
+++ analysis/src/index.ts
@@ -1,81 +1,124 @@
-import { join, dirname } from 'path';
-import { cwd } from 'process';
-import { Run } from './run';
-import { CanvasRenderService } from 'chartjs-node-canvas';
 import { ChartConfiguration } from 'chart.js';
+import { CanvasRenderService } from 'chartjs-node-canvas';
 import { writeFileSync } from 'fs';
+import { dirname, join } from 'path';
+import { Run } from './run';
 
-const graphRender = new CanvasRenderService(800, 400);
+// tslint:disable:no-console
 
 interface Data {
   producer: string;
   consumer: string;
 }
 
-type RunX5 = [Data, Data, Data, Data, Data];
-type Test = [RunX5, RunX5, RunX5, RunX5];
-
-// tslint:disable:no-console
-
-const file = process.argv.slice(2, 3)[0];
-const fileDir = dirname(file);
-// tslint:disable-next-line: no-var-requires
-const runFile: Test = require(join(cwd(), file));
-
-// normalise all paths
-runFile.forEach((run) => {
-  run.forEach((data) => {
-    data.producer = join(fileDir, data.producer);
-    data.consumer = join(fileDir, data.consumer);
-  });
-});
+type Test = Data[][];
 
 interface AveragedRun {
   loss: number;
   overhead: number;
 }
 
-// map each RunX5 into an AveragedRun.
-const avgs: AveragedRun[] = runFile.map((run) => {
-  const runs = run.map((d) => new Run(d.producer, d.consumer));
-  const loss = runs.reduce((acc, cur) => acc + cur.payloadLoss(), 0) / runs.length;
-  const overhead = runs.reduce((acc, cur) => acc + cur.overhead(), 0) / runs.length;
-  return { loss, overhead };
-});
+const graphRender = new CanvasRenderService(1000, 400);
+const logRoot = join(__dirname, '..', '..', 'logs');
 
-const chartConfig: ChartConfiguration = {
-  type: 'line',
-  data: {
-    labels: ['Normal', 'Acceptable', 'Degraded', 'Horrible'],
-    datasets: [
-      {
-        label: 'Overhead',
-        data: avgs.map((a) => a.overhead),
-        fill: false,
-        borderColor: '#fcad00'
-      },
-      {
-        label: 'Loss',
-        data: avgs.map((a) => 1 - a.loss),
-        fill: false,
-        borderColor: '#3d9be3'
-      }
-    ]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-        }
-      }]
-    }
-  }
+function getStats(file: string): AveragedRun[] {
+  const fileDir = dirname(file);
+  // tslint:disable-next-line: no-var-requires
+  const runFile: Test = require(file);
+
+  // normalise all paths
+  runFile.forEach((run) => {
+    run.forEach((data) => {
+      data.producer = join(fileDir, data.producer);
+      data.consumer = join(fileDir, data.consumer);
+    });
+  });
+
+  // map each run of the test into an AveragedRun.
+  const avgs: AveragedRun[] = runFile.map((run) => {
+    const runs = run.map((d) => new Run(d.producer, d.consumer));
+    const loss = runs.reduce((acc, cur) => acc + cur.payloadLoss(), 0) / runs.length;
+    const overhead = runs.reduce((acc, cur) => acc + cur.overhead(), 0) / runs.length;
+    return { loss, overhead };
+  });
+  return avgs;
 }
 
-console.log(avgs);
-console.log('rendering to graph.');
-graphRender.renderToBuffer(chartConfig, 'image/png').then((buffer) => {
-  writeFileSync(join(fileDir, 'graph.png'), buffer);
-  console.log('done');
-});
+function createGraph(figNum: number, protocol: string, avg512: AveragedRun[], avg64: AveragedRun[]): void {
+  const chartConfig: ChartConfiguration = {
+    type: 'line',
+    data: {
+      labels: ['Normal', 'Acceptable', 'Degraded', 'Horrible'],
+      datasets: [
+        {
+          label: 'Overhead at 512 bytes',
+          data: avg512.map((a) => a.overhead),
+          fill: false,
+          borderDash: [10, 5],
+          borderColor: '#fcad00'
+        },
+        {
+          label: 'Overhead at 64 bytes',
+          data: avg64.map((a) => a.overhead),
+          fill: false,
+          borderColor: '#f05316'
+        },
+        {
+          label: 'Payload bytes received at 512 bytes',
+          data: avg512.map((a) => 1 - a.loss),
+          fill: false,
+          borderDash: [10, 5],
+          borderColor: '#3d9be3'
+        },
+        {
+          label: 'Payload bytes received at 64 bytes',
+          data: avg64.map((a) => 1 - a.loss),
+          fill: false,
+          borderColor: '#1838ed'
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        position: 'bottom',
+        fontColor: 'black',
+        text: `Figure ${figNum}: ${protocol.toUpperCase()} results.`
+      },
+      legend: {
+        labels: {
+          fontColor: 'black'
+        }
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            fontColor: 'black',
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            fontColor: 'black',
+            beginAtZero: true,
+            callback: (v: number) => (v * 100).toFixed(0) + '%',
+          }
+        }]
+      }
+    }
+  }
+
+  console.log(`[${protocol}] rendering graph.`);
+  graphRender.renderToBuffer(chartConfig, 'image/png').then((buffer) => {
+    writeFileSync(join(process.cwd(), `${protocol}-graph.png`), buffer);
+    console.log(`[${protocol}] done`);
+  });
+}
+
+const protocols = ['tcp', 'udp', 'srdp'];
+for (let i = 0; i < protocols.length; i++) {
+  const protocol = protocols[i];
+  console.log(`[${protocol}] reading logs`)
+  const avg512 = getStats(join(logRoot, 'stream', protocol, 'run.json'));
+  const avg64 = getStats(join(logRoot, 'iot', protocol, 'run.json'));
+  createGraph(i + 1, protocol, avg512, avg64);
+}
